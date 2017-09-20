a438f9bdfae7bf6c3b0347b2ba011a1ef788b27e
Unknown <arnaud.tlj@laposte.net>
2017-09-20 10:21:19 +0200

    creation d'une variable utilisée dans le template du menu

--- my-app/src/app/menu/menu.component.ts
+++ my-app/src/app/menu/menu.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  title: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.title = 'Livraison';
   }
 
 }
